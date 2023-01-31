c86bb047dfc56bea30d8b65eefa3216d3bf08504
quansenB <inaki.breinbauer@gmail.com>
2023-01-31 23:53:34 +0100

    first commit

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: '/:slug*',
+        destination: 'https://9fb28265c03642edb170cbf9b66089a1.eu-central-1.aws.cloud.es.io:9243/:slug*',
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
