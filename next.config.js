961d006ba95ef8f1e3b4ff68a3a1b280e002fc61
quansenB <inaki.breinbauer@gmail.com>
2023-02-27 07:11:30 +0100

    Add permanent

--- next.config.js
+++ next.config.js
@@ -6,26 +6,31 @@ const nextConfig = {
         source: "/marketing-monday/zoom",
         destination:
           "https://schab-holding.zoom.us/j/89382276677?pwd=K084YkE4QmFGdmdUNEJyTnpuWlMzdz09",
+        permanent: true,
       },
       {
         source: "/marketing-monday/asana",
         destination:
           "https://app.asana.com/read-only/M-M-Protokoll/470860460277661/cd823fa58ce24182d4c3606c77e66382/list",
+        permanent: true,
       },
       {
         source: "/marketing-monday/kpi",
         destination:
           "https://docs.google.com/spreadsheets/d/1iaRQXr6bAcFuk9FJSjmtAx8DenxLiW4r0BQsq5sBQNw/edit#gid=0",
+        permanent: true,
       },
       {
         source: "/copywriting/asana",
         destination:
           "https://app.asana.com/read-only/CW-M-Protokoll/470860460277661/59ab03b543307e56960d8a08c1a5e69c/list",
+        permanent: true,
       },
       {
         source: "/copywriting/zoom",
         destination:
           "https://schab-holding.zoom.us/j/8951914236?pwd=Wm11cmozSjk0ckFxamJuWERBejlPQT09",
+        permanent: true,
       },
     ];
   },
