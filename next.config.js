4e1794d5449a86dab3951d904a5ce2cd586828d5
quansenB <inaki.breinbauer@gmail.com>
2023-04-01 22:47:51 +0200

    reenable elastic rewrites

--- next.config.js
+++ next.config.js
@@ -45,22 +45,22 @@ const nextConfig = {
       },
     ];
   },
-  // async rewrites() {
-  //   return {
-  //     beforeFiles: [
-  //       {
-  //         source: "/:slug*",
-  //         destination:
-  //           "https://9fb28265c03642edb170cbf9b66089a1.eu-central-1.aws.cloud.es.io:9243/:slug*",
-  //       },
-  //       {
-  //         source: "/",
-  //         destination:
-  //           "https://9fb28265c03642edb170cbf9b66089a1.eu-central-1.aws.cloud.es.io:9243",
-  //       },
-  //     ],
-  //   };
-  // },
+  async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: "/:slug*",
+          destination:
+            "https://9fb28265c03642edb170cbf9b66089a1.eu-central-1.aws.cloud.es.io:9243/:slug*",
+        },
+        {
+          source: "/",
+          destination:
+            "https://9fb28265c03642edb170cbf9b66089a1.eu-central-1.aws.cloud.es.io:9243",
+        },
+      ],
+    };
+  },
 };
 
 module.exports = nextConfig;
